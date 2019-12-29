1ab3310a34e1655761b9af9a04fbe6a180c116c8
Ashish Singh <ashishsingh@AshishSingh-2.local>
2019-12-29 17:23:42 +0530

    deleting old version

--- README.md
+++
@@ -1,2 +0,0 @@
-# travelApp
-Mobile APP
--- hotel/src/main/java/com/project/hotel/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.hotel.entity.RegisterProperty;
-
-public interface PropertyRepository extends JpaRepository<RegisterProperty, String>{
-
-}
